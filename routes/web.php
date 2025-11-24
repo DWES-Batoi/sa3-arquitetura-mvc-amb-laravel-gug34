c581a63ec4ffa0d05acbcb25ec15466bae2f79a0
gug34 <gutionil1@gmail.com>
2025-11-24 20:14:04 +0100

    MOD Rutas optimizadas

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,10 @@ Route::get('/', fn() => "Benvingut a la Guia d'Equips de Futbol Femení!");
 Route::resource('equips', EquipController::class);
 
 // Estadis
-Route::get('/estadis', [EstadiController::class, 'index'])->name('estadis.index');
-Route::get('/estadis/crear', [EstadiController::class, 'create'])->name('estadis.create');
-Route::post('/estadis', [EstadiController::class, 'store'])->name('estadis.store');
-Route::get('/estadis/{id}', [EstadiController::class, 'show'])->name('estadis.show');
+Route::resource('estadis', EstadiController::class);
 
 // Jugadores 
-Route::get('/jugadores', [JugadoraController::class, 'index'])->name('jugadores.index');
-Route::get('/jugadores/crear', [JugadoraController::class, 'create'])->name('jugadores.create');
-Route::post('/jugadores', [JugadoraController::class, 'store'])->name('jugadores.store');
-Route::get('/jugadores/{id}', [JugadoraController::class, 'show'])->name('jugadores.show');
+Route::resource('jugadores', JugadoraController::class);
 
 // Partits
-Route::get('/partits', [PartitController::class, 'index'])->name('partits.index');
-Route::get('/partits/crear', [PartitController::class, 'create'])->name('partits.create');
-Route::post('/partits', [PartitController::class, 'store'])->name('partits.store');
-Route::get('/partits/{id}', [PartitController::class, 'show'])->name('partits.show');
+Route::resource('partits', PartitController::class);
